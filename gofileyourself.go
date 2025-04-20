a684f3c9b0eaa8d9fbe8f7beb89386be5c2b24c3
Thilo Bronnenmeyer <t.bronnenmeyer@gmail.com>
2025-04-20 10:59:47 +0200

    patch: refactor to oop pattern

--- gofileyourself.go
+++ gofileyourself.go
@@ -1,33 +1,67 @@
 package main
 
 import (
-	"github.com/rivo/tview"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"github.com/gdamore/tcell/v2"
+  "github.com/rivo/tview"
+  "os"
+  "os/exec"
+  "path/filepath"
+  "github.com/gdamore/tcell/v2"
+	"log"
 )
 
-func loadDirectory(path string) (*tview.List, error) {
-	file_info, err := os.Stat(path)
-	if err != nil {
-		return nil, err
-	}
-	if file_info.IsDir() {
-		files, err := os.ReadDir(path)
-		if err != nil {
-			return nil, err
-		}
-		list := tview.NewList().ShowSecondaryText(false)
-		for _, file := range files {
-			list.AddItem(file.Name(), "", 0, nil)
-		}
-		return list, nil
-	} else {
-		return nil, nil
-	}
+// FileExplorer represents the state and behavior of the file explorer
+type FileExplorer struct {
+  app                  *tview.Application
+  currentPath         string
+  currentList         *tview.List
+  parentList          tview.Primitive
+  selectedList        tview.Primitive
+  flex                *tview.Flex
+  directoryToIndexMap map[string]int
 }
 
+// NewFileExplorer creates and initializes a new FileExplorer
+func NewFileExplorer() (*FileExplorer, error) {
+  currentPath, err := os.Getwd()
+  if err != nil {
+      return nil, err
+  }
+
+  fe := &FileExplorer{
+      app:                  tview.NewApplication(),
+      currentPath:         currentPath,
+      directoryToIndexMap: make(map[string]int),
+      flex:                tview.NewFlex(),
+  }
+
+  if err := fe.initialize(); err != nil {
+      return nil, err
+  }
+
+  return fe, nil
+}
+
+// loadDirectory is a helper function that loads directory contents into a list
+func loadDirectory(path string) (*tview.List, error) {
+    fileInfo, err := os.Stat(path)
+    if err != nil {
+        return nil, err
+    }
+    if fileInfo.IsDir() {
+        files, err := os.ReadDir(path)
+        if err != nil {
+            return nil, err
+        }
+        list := tview.NewList().ShowSecondaryText(false)
+        for _, file := range files {
+            list.AddItem(file.Name(), "", 0, nil)
+        }
+        return list, nil
+    }
+    return nil, nil
+}
+
+// loadFilePreview is a helper function that creates a text view for file contents
 func loadFilePreview(path string) (*tview.TextView, error) {
     content, err := os.ReadFile(path)
     if err != nil {
@@ -43,8 +77,8 @@ func loadFilePreview(path string) (*tview.TextView, error) {
     return textView, nil
 }
 
+// openInNvim is a helper function that opens a file in neovim
 func openInNvim(path string, app *tview.Application) error {
-    // Suspend the terminal UI
     app.Suspend(func() {
         cmd := exec.Command("nvim", path)
         cmd.Stdin = os.Stdin
@@ -52,167 +86,210 @@ func openInNvim(path string, app *tview.Application) error {
         cmd.Stderr = os.Stderr
         cmd.Run()
     })
-    
     return nil
 }
 
+// initialize sets up the initial state of the FileExplorer
+func (fe *FileExplorer) initialize() error {
+  var err error
+  fe.currentList, err = loadDirectory(fe.currentPath)
+  if err != nil {
+      return err
+  }
+
+  parentPath := filepath.Join(fe.currentPath, "..")
+	newParentList, err := loadDirectory(parentPath)
+  if err != nil {
+      return err
+  }
+  parentDirectoryIndex := 0
+  for i := 0; i < newParentList.GetItemCount(); i++ {
+      if text, _ := newParentList.GetItemText(i); text == filepath.Base(fe.currentPath) {
+          parentDirectoryIndex = i
+          break
+      }
+  }
+  newParentList.SetCurrentItem(parentDirectoryIndex)
+	fe.parentList = newParentList
+  parentAbsolutePath, _ := filepath.Abs(parentPath)
+  fe.directoryToIndexMap[parentAbsolutePath] = parentDirectoryIndex
+
+  selectedName, _ := fe.currentList.GetItemText(0)
+  selectedPath := filepath.Join(fe.currentPath, selectedName)
+  fe.selectedList, err = loadDirectory(selectedPath)
+  if err != nil {
+      return err
+  }
+
+  fe.setupKeyBindings()
+  fe.draw()
+  return nil
+}
+
+// draw updates the UI
+func (fe *FileExplorer) draw() {
+    fe.flex.Clear()
+    if fe.parentList != nil {
+        fe.flex.AddItem(fe.parentList, 0, 1, false)
+    }
+    if fe.currentList != nil {
+        fe.flex.AddItem(fe.currentList, 0, 2, true)
+    }
+    if fe.selectedList != nil {
+        fe.flex.AddItem(fe.selectedList, 0, 2, false)
+    }
+    fe.app.SetRoot(fe.flex, true)
+}
+
+// updateSelectedDirectory updates the selected directory/file preview
+func (fe *FileExplorer) updateSelectedDirectory(selectedPath string) error {
+  selectedAbsolutePath, _ := filepath.Abs(selectedPath)
+  selectedDirectoryIndex := fe.directoryToIndexMap[selectedAbsolutePath]
+    
+  newSelectedList, err := loadDirectory(selectedPath)
+  if err != nil {
+      return err
+  }
+
+  if newSelectedList == nil {
+      fe.selectedList, err = loadFilePreview(selectedPath)
+      if err != nil {
+          return err
+      }
+  } else {
+      newSelectedList.SetCurrentItem(selectedDirectoryIndex)
+      fe.selectedList = newSelectedList
+  }
+
+  fe.draw()
+  return nil
+}
+
+// changeCurrentDirectory changes the current directory and updates related views
+func (fe *FileExplorer) changeCurrentDirectory(path string) error {
+  // Update current directory
+  currentAbsolutePath, _ := filepath.Abs(path)
+  currentDirectoryIndex := fe.directoryToIndexMap[currentAbsolutePath]
+  newCurrentList, err := loadDirectory(path)
+  if err != nil {
+      return err
+  }
+  
+  newCurrentList.SetInputCapture(fe.currentList.GetInputCapture())
+  newCurrentList.SetCurrentItem(currentDirectoryIndex)
+  fe.currentList = newCurrentList
+
+  // Update parent directory
+	if currentAbsolutePath == "/" {
+		emptyList := tview.NewList().ShowSecondaryText(false)
+		fe.parentList = emptyList
+	} else {
+  	parentPath := filepath.Join(path, "..")
+  	newParentList, err := loadDirectory(parentPath)
+  	if err != nil {
+  	    return err
+  	}
+
+  	parentDirectoryIndex := 0
+  	for i := 0; i < newParentList.GetItemCount(); i++ {
+  	    if text, _ := newParentList.GetItemText(i); text == filepath.Base(path) {
+  	        parentDirectoryIndex = i
+  	        break
+  	    }
+  	}
+  	
+  	parentAbsolutePath, _ := filepath.Abs(parentPath)
+  	fe.directoryToIndexMap[parentAbsolutePath] = parentDirectoryIndex
+  	newParentList.SetCurrentItem(parentDirectoryIndex)
+  	fe.parentList = newParentList
+	}
+
+  // Update selected directory
+  selectedName, _ := fe.currentList.GetItemText(currentDirectoryIndex)
+  selectedPath := filepath.Join(path, selectedName)
+  if err := fe.updateSelectedDirectory(selectedPath); err != nil {
+      return err
+  }
+
+  fe.currentPath = path
+  return nil
+}
+
+// updateCurrentLine updates the current line selection
+func (fe *FileExplorer) updateCurrentLine(lineIndex int) error {
+	  if lineIndex < 0 || lineIndex >= fe.currentList.GetItemCount() {
+        return nil
+    }
+    fe.currentList.SetCurrentItem(lineIndex)
+    currentAbsolutePath, _ := filepath.Abs(fe.currentPath)
+    fe.directoryToIndexMap[currentAbsolutePath] = lineIndex
+    
+    selectedName, _ := fe.currentList.GetItemText(lineIndex)
+    return fe.updateSelectedDirectory(filepath.Join(fe.currentPath, selectedName))
+}
+
+// setupKeyBindings configures keyboard input handling
+func (fe *FileExplorer) setupKeyBindings() {
+    fe.currentList.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+        switch event.Rune() {
+        case 'j': // scroll down
+            fe.updateCurrentLine(fe.currentList.GetCurrentItem() + 1)
+            return nil
+        case 'k': // scroll up
+            fe.updateCurrentLine(fe.currentList.GetCurrentItem() - 1)
+            return nil
+        case 'q': // quit
+            fe.app.Stop()
+            return nil
+        case 'l': // open dir or file
+            currentItem := fe.currentList.GetCurrentItem()
+            fileName, _ := fe.currentList.GetItemText(currentItem)
+            filePath := filepath.Join(fe.currentPath, fileName)
+            fileInfo, err := os.Stat(filePath)
+            if err != nil {
+                return event
+            }
+            if fileInfo.IsDir() {
+                if err := fe.changeCurrentDirectory(filePath); err != nil {
+                    return event
+                }
+            } else {
+                openInNvim(filePath, fe.app)
+                return nil
+            }
+            return nil
+        case 'h': // go up directory
+            dirPath := filepath.Join(fe.currentPath, "..")
+            if err := fe.changeCurrentDirectory(dirPath); err != nil {
+                return event
+            }
+            return nil
+        }
+        return event
+    })
+}
+
+// Run starts the file explorer
+func (fe *FileExplorer) Run() error {
+    return fe.app.SetRoot(fe.flex, true).Run()
+}
+
 func main() {
-	app := tview.NewApplication()
-	directory_to_index_map := make(map[string]int)
+    logFile, err := os.OpenFile("debug.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+    if err != nil {
+        log.Fatalf("Error opening log file: %v", err)
+    }
+    
+    // Configure log package to write to file
+    log.SetOutput(logFile)
+    // Optional: include file and line number in logs
+    log.SetFlags(log.Ltime | log.Lshortfile)
+    fe, err := NewFileExplorer()
+    if err != nil {
+        panic(err)
+    }
 
-	// current directory
-	current_path, err := os.Getwd()
-	current_list, err := loadDirectory(current_path)
-	if err != nil {
-		panic(err)
-	}
-
-	// parent directory
-	parent_path := filepath.Join(current_path, "..")
-	parent_list, err := loadDirectory(parent_path)
-	if err != nil {
-		panic(err)
-	}
-
-	// selected directory
-	var selected_list tview.Primitive
-	selected_name, _ := current_list.GetItemText(0)
-	selected_path := filepath.Join(current_path, selected_name)
-	selected_list, err = loadDirectory(selected_path)
-	if err != nil {
-		panic(err)
-	}
-
-	flex := tview.NewFlex()
-
-	draw := func() {
-		flex.Clear()
-		if parent_list != nil {
-			flex.AddItem(parent_list, 0, 1, false)
-		}
-		if current_list != nil {
-			flex.AddItem(current_list, 0, 1, true)
-		}
-		if selected_list != nil {
-			flex.AddItem(selected_list, 0, 1, false)
-		}
-		app.SetRoot(flex, true)
-	}
-	draw()
-
-
-	updateSelectedDirectory := func(selected_path string, event *tcell.EventKey) (*tcell.EventKey, error) {
-		selected_absolute_path, _ := filepath.Abs(selected_path)
-		selected_directory_index := directory_to_index_map[selected_absolute_path]
-		new_selected_list, err := loadDirectory(selected_path)
-		if err != nil {
-			return nil, err
-		}
-		if new_selected_list == nil {
-			selected_list, _ = loadFilePreview(selected_path)
-			draw()
-			return event, nil
-		}
-		new_selected_list.SetCurrentItem(selected_directory_index)
-		if err != nil {
-			return nil, err
-		}
-		selected_list = new_selected_list
-		draw()
-		return event, nil
-	}
-
-	changeCurrentDirectory := func(path string, event *tcell.EventKey) (*tcell.EventKey, error) {
-		// current directory
-		current_absolute_path, _ := filepath.Abs(path)
-		current_directory_index := directory_to_index_map[current_absolute_path]
-		new_current_list, err := loadDirectory(path)
-		new_current_list.SetInputCapture(current_list.GetInputCapture())
-		new_current_list.SetCurrentItem(current_directory_index)
-		current_list = new_current_list
-
-		// parent directory
-		parent_path := filepath.Join(path, "..")
-		new_parent_list, err := loadDirectory(parent_path)
-		if err != nil {
-			return nil, err
-		}
-		parent_directory_index := 0
-		for i := 0; i < new_parent_list.GetItemCount(); i++ {
-			if text, _ := new_parent_list.GetItemText(i); text == filepath.Base(path) {
-				parent_directory_index = i
-				break
-			}
-		}
-		new_parent_list.SetCurrentItem(parent_directory_index)
-		parent_list = new_parent_list
-
-
-		// selected directory
-		selected_name, _ := current_list.GetItemText(current_directory_index)
-		selected_path := filepath.Join(path, selected_name)
-		updateSelectedDirectory(selected_path, event)
-		current_path = path
-		
-		return event, nil
-	}
-
-	updateCurrentLine := func(line_index int, event *tcell.EventKey) (*tcell.EventKey, error) {
-		current_list.SetCurrentItem(line_index)
-		current_absolute_path, _ := filepath.Abs(current_path)
-		directory_to_index_map[current_absolute_path] = line_index
-		selected_name, _ := current_list.GetItemText(line_index)
-		updateSelectedDirectory(filepath.Join(current_path, selected_name), event)
-		return event, nil
-	}
-
-	if _, err := changeCurrentDirectory(current_path, nil); err != nil {
-		panic(err)
-	}
-
-	current_list.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		switch event.Rune() {
-		case 'j': // scroll down
-			updateCurrentLine(current_list.GetCurrentItem() + 1, event)
-			return nil
-		case 'k': // scroll up
-			updateCurrentLine(current_list.GetCurrentItem() - 1, event)
-			return nil
-		case 'q': // quit
-			app.Stop()
-			return nil
-		case 'l': // open dir or file
-			current_item := current_list.GetCurrentItem()
-			file_name, _ := current_list.GetItemText(current_item)
-			file_path := filepath.Join(current_path, file_name)
-			file_info, err := os.Stat(file_path)
-			if err != nil {
-				return event
-			}
-			if file_info.IsDir() {
-				event, err := changeCurrentDirectory(file_path, event)
-				if err != nil {
-					return event
-				}
-			} else {
-				openInNvim(file_path, app)
-				return nil
-			}
-			current_path = file_path
-			return nil
-		case 'h': // go up directory
-			dir_path := filepath.Join(current_path, "..")
-			event, err := changeCurrentDirectory(dir_path, event)
-			if err != nil {
-				return event
-			}
-			current_path = dir_path
-			return nil
-		}
-		return event
-	})
-
-	if err := app.SetRoot(flex, true).Run(); err != nil {
-		panic(err)
-	}
+    if err := fe.Run(); err != nil {
+        panic(err)
+    }
 }
