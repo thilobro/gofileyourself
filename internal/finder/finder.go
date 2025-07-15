caf51bed4e1665f741ac1cc1df57f6268a6b885e
Thilo Bronnenmeyer <t.bronnenmeyer@gmail.com>
2025-07-15 14:09:33 +0200

    patch: clean up keybindings (#73)
    Co-authored-by: Thilo Bronnenmeyer <thilo.bronnenmeyer@wemolo.com>

--- internal/finder/finder.go
+++ internal/finder/finder.go
@@ -126,64 +126,6 @@ func (finder *Finder) setSelectedDirectory(selectedPath string) error {
 	return nil
 }
 
-func (finder *Finder) SetupKeyBindings() {
-	finder.rootFlex.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		defer finder.Draw()
-		switch event.Key() {
-		case tcell.KeyCtrlR:
-			finder.showRecentHistory()
-			return nil
-		case tcell.KeyCtrlG:
-			finder.showGrep()
-			return nil
-		case tcell.KeyCtrlH:
-			finder.context.ShowHiddenFiles = !finder.context.ShowHiddenFiles
-
-			// Remember current selection before refresh
-			_, currentName := finder.searchedList.GetItemText(finder.searchedList.GetCurrentItem())
-			finder.resetFileList(false)
-			helper.CopyListContent(finder.fileList, finder.searchedList)
-
-			// Restore current selection
-			if idx := helper.FindExactItem(finder.searchedList, currentName); idx >= 0 {
-				finder.setCurrentLine(idx)
-			}
-			go finder.manageFuzzySearch(finder.searchTerm)
-			return nil
-		case tcell.KeyUp:
-			finder.setCurrentLine(finder.searchedList.GetCurrentItem() - 1)
-			return nil
-		case tcell.KeyDown:
-			finder.setCurrentLine(finder.searchedList.GetCurrentItem() + 1)
-			return nil
-		case tcell.KeyEnter:
-			currentItem := finder.searchedList.GetCurrentItem()
-			_, fileName := finder.searchedList.GetItemText(currentItem)
-			filePath := helper.GetAbsFilePath(fileName, finder.context.CurrentPath)
-
-			fileInfo, err := os.Stat(filePath)
-			if err != nil {
-				return event
-			}
-
-			if fileInfo.IsDir() {
-				finder.context.CurrentPath = filePath
-				finder.context.OnWidgetResult(widget.Find, filePath)
-				return nil
-			}
-			helper.OpenInNvim(filePath, finder.context.ChooseFilePath, finder.context.App, finder.context.Config.HistoryLen)
-			return nil
-		}
-
-		if finder.footer == nil {
-			finder.handleFooterInput()
-		}
-		finder.currentFocusedWidget = finder.footer
-		finder.footer.GetInputCapture()(event)
-		return nil
-	})
-}
-
 func (finder *Finder) handleFooterInput() {
 	finder.footer = tview.NewInputField().SetText("/")
 	finder.footer.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
